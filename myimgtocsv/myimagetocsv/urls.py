b5511d0a8759e47fedd7e8beb302394287d62086
Darshan Sathish Shetty <darshanshetty253101@gmail.com>
2024-09-26 05:27:00 +0530

    added csv reading function at /upload

--- myimgtocsv/myimagetocsv/urls.py
+++ myimgtocsv/myimagetocsv/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("test", views.index, name="index"),
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("upload", views.upload_file, name="upload_file")
 ]
--- myimgtocsv/myimagetocsv/views.py
+++ myimgtocsv/myimagetocsv/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from PIL import Image
 import pytesseract
 
+import csv
+from .forms import UploadFileForm
+
+
 def index(request):
     outstring = pytesseract.image_to_string(Image.open('test/cropped4.jpeg'))
     return HttpResponse(outstring)
@@ -13,3 +17,25 @@ def home(request):
         'content': 'Welcome to the Home Page!'
     }
     return render(request, 'home.html', context)
+
+uploaded_files = []
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # Get the uploaded file
+            file = request.FILES['file']
+            # Decode the file (assuming UTF-8 encoded CSV)
+            decoded_file = file.read().decode('utf-8').splitlines()
+            reader = csv.reader(decoded_file)
+            
+            # Get the first row (header or first data row)
+            first_row = next(reader)  # This will return the first row as a list
+            
+            # Return the first row as an HTTP response
+            return HttpResponse(f"First row of CSV: {first_row}")
+    else:
+        form = UploadFileForm()
+
+    return render(request, 'upload.html', {'form': form})
