6576a8cdd136ab1bcb193bdb64416fb98ca88fe5
Darshan Sathish Shetty <darshanshetty253101@gmail.com>
2024-09-27 21:02:02 +0530

    added basic frontend with css

--- myimgtocsv/myimgtocsv/settings.py
+++ myimgtocsv/myimgtocsv/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -115,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Add this if it's not already present
+STATICFILES_DIRS = [
+    BASE_DIR /"static",  # Adjust to your base project directory
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
